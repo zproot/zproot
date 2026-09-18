b96c78ea8e2c5856616d9a7033bbdb31fe2e05d9
Frostre_ <n9043395@gmail.com>
2026-09-18 20:30:01 +0200

    Refactor cstring function and add word function

--- src/memory/write.zig
+++ src/memory/write.zig
@@ -7,15 +7,19 @@ fn poke(pid: types.pid_t, addr: u64, data: u64) !void {
 }
 
 pub fn cstring(pid: types.pid_t, addr: u64, str: []const u8) !void {
-    var word: u64 = 0;
+    var w: u64 = 0;
     var i: usize = 0;
     while (i <= str.len) : (i += 1) {
         const byte: u8 = if (i < str.len) str[i] else 0;
-        word |= @as(u64, byte) << @intCast((i % 8) * 8);
+        w |= @as(u64, byte) << @intCast((i % 8) * 8);
         if (i % 8 == 7 or i == str.len) {
             const base = addr + (i - (i % 8));
-            try poke(pid, base, word);
-            word = 0;
+            try poke(pid, base, w);
+            w = 0;
         }
     }
+}
+
+pub fn word(pid: types.pid_t, addr: u64, data: u64) !void {
+    try poke(pid, addr, data);
 }
